0767ef7f2ee561588be3d45b079d3c7462522dbe
shikeik <30600841+shikeik@users.noreply.github.com>
2024-03-26 19:15:02 +0800

    添加项目文件。

---
+++ Scripts/Runtime/InputSystem/Basics/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GoldSprite.UnityPlugins.MyInputSystem {
+    public class BasicRoleInputs : MyInputManager {
+
+    }
+}
---
+++ Scripts/Runtime/InputSystem/MyInputManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GoldSprite.UnityPlugins.MyInputSystem {
+    public class MyInputManager : MonoBehaviour {
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
